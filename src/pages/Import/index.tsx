ebd6084f2002c76a7e121b305f1c925fa1a8d8ed
Augusto Cavalca <guto.cavalca@gmail.com>
2020-06-18 16:59:11 -0300

    chore: Implementação do desafio gofinances

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    const fileCVS = uploadedFiles[0];
+
+    data.append('file', fileCVS.file, fileCVS.file.name);
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileCVS = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles(fileCVS);
   }
 
   return (
